8597111a92330134078a3385aca7a17009955c79
duongngoctuan571-jpg <duongngoctuan571@gmail.com>
2026-01-05 13:40:00 +0700

    Update TanPhatHUDv7.8.lua

--- TanPhatHUDv7.8.lua
+++ TanPhatHUDv7.8.lua
@@ -966,6 +966,45 @@ local targetPlayers = {
 	["layandikit12"] = true,
 	["Sammy"] = true
 }
+
+local TeleportService = game:GetService("TeleportService")
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+
+local PlaceId = game.PlaceId
+local JobId = game.JobId
+
+function Hop()
+	local servers = {}
+	local cursor = ""
+
+	for i = 1, 3 do -- thử tối đa 3 lần
+		local url = "https://games.roblox.com/v1/games/"..PlaceId.."/servers/Public?sortOrder=Asc&limit=100"
+		if cursor ~= "" then
+			url = url .. "&cursor=" .. cursor
+		end
+
+		local req = game:HttpGet(url)
+		local data = HttpService:JSONDecode(req)
+
+		for _, server in pairs(data.data) do
+			if server.playing < server.maxPlayers and server.id ~= JobId then
+				table.insert(servers, server.id)
+			end
+		end
+
+		if data.nextPageCursor then
+			cursor = data.nextPageCursor
+		else
+			break
+		end
+	end
+
+	if #servers > 0 then
+		TeleportService:TeleportToPlaceInstance(PlaceId, servers[math.random(1, #servers)], Players.LocalPlayer)
+	end
+end
+
 spawn(function()
 	while true do
 		wait(1)
